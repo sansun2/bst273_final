2b88f90925830dc2b39e3306b98d8bb86c016076
Sanjana Sundaresan <ssundaresan@hsph.harvard.edu>
2018-10-23 12:03:09 -0400

    Able to plot unstratified plot.

--- scatter.py
+++ scatter.py
@@ -3,17 +3,18 @@
 BST 273 Final Project, 100 points
 =================================
 
-NAME:
+NAME: Sanjana Sundaresan
 EMAIL:
 
 """
 
 import argparse
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn
 
-
-parser = argparse.ArgumentParser( description=description )
+parser = argparse.ArgumentParser( description="" )
 parser.add_argument(
 	"input_file",
 	help="path to the input data file (TSV format)",
@@ -31,17 +32,11 @@ parser.add_argument(
 	default=1,
 	help="1-based index of the y-column to work on",
 )
-parser.add_argument(
-	"-z", "--zcol",
-	type=int,
-	default=1,
-	help="1-based index of the z-column to work on",
-)
 
 parser.add_argument(
-	"-z", "--zcol",
+	"-strat", "--strat",
 	type=int,
-	default=1,
+	default=None,
 	help="1-based index of the z-column to work on",
 )
 
@@ -55,7 +50,28 @@ Need to add in another argument for the path to the output file.
 args = parser.parse_args( )
 
 fh = open(args.input_file)
+df = pd.read_csv(args.input_file, delimiter = '\t')
+headers = np.array(df.columns)  # get headers
 
-data= data.split("\t")
+a = args.xcol -1
+b = args.ycol -1
+cc = args.strat -1
 
-x =
+row = []
+x = []
+y = []
+z = []
+
+df.groupby(headers[args.strat-1])
+for line in fh:
+    if "#" not in line: #removes the header row
+        row = line.strip().split("\t") #first split into individual rows
+        x.append(float(row[args.xcol -1]))
+        y.append(float(row[args.ycol -1]))
+        z.append(row[args.strat -1])
+
+
+plt.scatter(x,y, label = z)
+plt.xlabel(headers[a])
+plt.ylabel(headers[b])
+plt.show()
